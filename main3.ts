dedaafdded8163185a4cf3e2afa0af5520999d82
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-26 19:38:44 -0300

    Third recorded class

---
+++ main3.ts
@@ -0,0 +1,14 @@
+// Pascal Case when using interfaces
+interface Point {
+    x: number,
+    y: number
+}
+
+let drawPoint = (point: Point) => {
+    // ...
+}
+
+drawPoint({
+    x: 1,
+    y: 2
+})
